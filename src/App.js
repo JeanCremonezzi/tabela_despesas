7c9b9b926ae2bbc5c526d54aab5d6ff397736e83
JeanCremonezzi <jeancremonezzi@hotmail.com>
2021-06-04 06:55:06 -0300

    Componente ExpenseItem adicionado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const expenses = [
+		{ id: "expense0", title: "Car Insurance", amount: 300.0, date: new Date(2021, 5, 4) },
+		{ id: "expense1", title: "New TV", amount: 1000.0, date: new Date(2021, 6, 25) },
+		{ id: "expense2", title: "New Desk", amount: 399.99, date: new Date(2021, 5, 18) },
+	];
+
+	return (
+		<>
+			{expenses.map((expense) => {
+				return (
+					<ExpenseItem
+						key={expense.id}
+						title={expense.title}
+						amount={expense.amount}
+						date={expense.date}
+					/>
+				);
+			})}
+		</>
+	);
 }
 
 export default App;
