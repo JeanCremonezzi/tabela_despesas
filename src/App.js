8033c3519173aceba42c01444b6dc33baa703e80
JeanCremonezzi <jeancremonezzi@hotmail.com>
2021-07-19 18:46:25 -0300

    Adicionar e Renderizar Expenses na lista

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
+import {useState} from 'react';
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
+function App() {	
+	const [expenses, setExpenses] = useState([])
+
 	const addNewExpenseHandler = (expense) => {
 		expense = { ...expense, id: expenses.length + 1 };
-		console.log(expense);
-	};
 
-	const expenses = [
-		{
-			id: "expense0",
-			title: "Car Insurance",
-			amount: 300.0,
-			date: new Date(2021, 5, 4),
-		},
-		{
-			id: "expense1",
-			title: "New TV",
-			amount: 1000.0,
-			date: new Date(2021, 6, 25),
-		},
-		{
-			id: "expense2",
-			title: "New Desk",
-			amount: 399.99,
-			date: new Date(2021, 5, 18),
-		},
-	];
+		setExpenses((prevExpenses) => {
+			return [expense, ...prevExpenses];
+		});
+	};
 
 	return (
 		<main>
